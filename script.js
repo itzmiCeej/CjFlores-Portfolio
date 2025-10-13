e1957989ce91a167652978c400cceddc9e939607
unknown <cjflores0514@gmail.com>
2025-10-13 16:59:47 +0800

    Initial Commit

---
+++ script.js
@@ -0,0 +1,67 @@
+
+.login-form {
+    margin: 20vh;
+    background: rgba(0, 0, 0, 0.5);
+    border-radius: 30px;
+    padding: 4vh 5vh;
+    width: 100%;
+    max-width: 400px;
+}
+.login-top {
+    display: flex;
+    align-items: end;
+}
+.login-form img {
+    height: 150px;
+    animation: rotate 2s ease infinite;
+}
+.login-form h1 {
+    font-size: 3vh;
+    color: rgb(200, 20, 20);
+}
+.login-body {
+    margin-bottom: 3vh;
+}
+.login-body input {
+    width: 100%;
+    font-size: 1.5vh;
+    border: 1px solid black;
+    border-radius: 8px;
+    cursor: pointer;
+    outline: none;
+    padding: 14px 12px;
+    transform: .3s ease;
+    margin-bottom: 20px;
+}
+.login-body label {
+    color: rgb(200, 20, 20);
+}
+.login-btn {
+    padding: 10px 185px;
+    font-size: 2vh;
+    margin-top: 10px;
+    background-color: rgb(200, 20, 20);
+    border-radius: 8px;
+    color: white;
+}
+.login-btn:hover {
+    background-color: red;
+}
+.sign-up {
+    display: flex;
+    align-items: center;
+}
+.sign-up h2 {
+    color: white;
+    font-size: 2vh;
+}
+.sign-up a {
+    color: rgb(200, 20, 20);
+    font-size: 1.5vh;
+    text-decoration: none;
+    margin-left: 2vh;
+}
+.sign-up a:hover {
+    color: red;
+    text-decoration: underline;
+}
